39f57130b7d4d6cc6ab9f09a06a3a1a8449dcf53
shane-os <shaneosullivansos@yahoo.co.uk>
2021-01-31 17:40:40 +0000

    Added initial outline of fetch api method.

---
+++ index.js
@@ -0,0 +1,9 @@
+let website = "api.openweathermap.org/data/2.5/forecast/daily?q=London&appid=998966fabbbdab57f239ac694a7f1675";
+
+fetch(website)
+    .then(function(){
+
+    });
+    .catch(function(){
+
+    });
