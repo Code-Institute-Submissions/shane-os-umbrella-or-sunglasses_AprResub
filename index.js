7de11ab33d6ccdc6dc856194b3da770bb75e8e7b
shane-os <shaneosullivansos@yahoo.co.uk>
2021-01-31 17:51:33 +0000

    Converted fetched data to json.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let website = "api.openweathermap.org/data/2.5/forecast/daily?q=London&appid=998966fabbbdab57f239ac694a7f1675";
 
 fetch(website)
+    .then(response => response.json())
+
     .then(function(){
 
     });
