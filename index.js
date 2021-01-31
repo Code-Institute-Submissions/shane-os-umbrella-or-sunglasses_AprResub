f4a69e7a68b849624eca37ea18b79ceb7c4b0c87
shane-os <shaneosullivansos@yahoo.co.uk>
2021-01-31 18:21:43 +0000

    Changed website url to resolve issue

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let website = "api.openweathermap.org/data/2.5/forecast/daily?q=London&appid=998966fabbbdab57f239ac694a7f1675";
+let website = "api.openweathermap.org/data/2.5/forecast?q=London&appid=998966fabbbdab57f239ac694a7f1675";
 
 fetch(website)
     if (this.readyState == 4 && this.status == 200){
