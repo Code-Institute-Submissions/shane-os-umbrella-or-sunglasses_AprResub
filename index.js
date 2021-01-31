4ff89d667ef060991b1863388994eec384562345
shane-os <shaneosullivansos@yahoo.co.uk>
2021-01-31 18:14:29 +0000

    Added status check for fetch

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 let website = "api.openweathermap.org/data/2.5/forecast/daily?q=London&appid=998966fabbbdab57f239ac694a7f1675";
 
 fetch(website)
-    .then(response => response.json())
+    if (this.readyState == 4 && this.status == 200){
+        .then(response => response.json())
+    };
+        .then(function(weather){
+            let data = weather.results;
+            console.log(data);
+        });
+        .catch(function(){
 
-    .then(function(){
-
-    });
-    .catch(function(){
-
-    });
+        });
