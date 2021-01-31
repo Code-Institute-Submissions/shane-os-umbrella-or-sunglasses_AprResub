14eb6f29e5bf472a4568f49a8e123a39aaae86a2
shane-os <shaneosullivansos@yahoo.co.uk>
2021-01-31 16:54:44 +0000

    Added API key to XML request

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var xmlrequest = new XMLHttpRequest();
 
+xmlrequest.open("GET", "api.openweathermap.org/data/2.5/forecast/daily?id=London&appid=998966fabbbdab57f239ac694a7f1675")
 xmlrequest.onreadystatechange = function() {
     if(this.readyState == 4 && this.statusText == 200) {
         document.getElementById("weatherdata").innerHTML = this.responseText;
