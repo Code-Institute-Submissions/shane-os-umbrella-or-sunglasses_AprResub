127ce6e2273f8c0d80526723d1cfc528c6b7e18b
shane-os <shaneosullivansos@yahoo.co.uk>
2021-01-31 18:50:06 +0000

    Built function structure.

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 let website = "api.openweathermap.org/data/2.5/forecast?q=London&appid=998966fabbbdab57f239ac694a7f1675";
 
-fetch(website)
+
+function getData(){
+    fetch(website)
     if (this.readyState == 4 && this.status == 200){
         .then(response => response.json())
     };
-        .then(function(weather){
-            let data = weather.results;
-            console.log(data);
-            return data;
-        });
-        .catch(function(error){
-            console.log(error);
-        });
+    .then(function(weather){
+        let data = weather.results;
+        console.log(data);
+        return data;
+    });
+    .catch(function(error){
+        console.log(error);
+    });
+}
