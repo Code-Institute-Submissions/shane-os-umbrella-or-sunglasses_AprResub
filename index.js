4d93f0e33275c9fb6134365660512d217f0de877
shane-os <shaneosullivansos@yahoo.co.uk>
2021-02-06 16:15:09 +0000

    Added weather icons to weather display

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function displayData(data) {
     document.getElementById('cityname').innerHTML = data.name;
     document.getElementById('countryname').innerHTML = data.sys.country;
     document.getElementById('citydescription').innerHTML = data.weather[0].description;
+    document.getElementById('weatherconditionsicon').innerHTML = "<img src="+"http://openweathermap.org/img/wn/"+data.weather[0].icon+".png>";
     document.getElementById('citywind').innerHTML = data.wind.speed;
     temperature = parseFloat(data.main.temp) -273.15;
     document.getElementById('citytemp').innerHTML = temperature.toFixed(2)+" Degrees Celcius";
